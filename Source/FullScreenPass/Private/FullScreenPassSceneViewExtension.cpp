8d020cbc1c05a100b1e72954daa4a4c4c23c5eb9
4rni4ka <basicc.tea@gmail.com>
2026-04-17 23:06:52 +0300

    Update FullScreenPassSceneViewExtension.cpp

--- Source/FullScreenPass/Private/FullScreenPassSceneViewExtension.cpp
+++ Source/FullScreenPass/Private/FullScreenPassSceneViewExtension.cpp
@@ -5,7 +5,6 @@
 #include "PostProcess/PostProcessInputs.h"
 #include "DynamicResolutionState.h"
 
-
 static TAutoConsoleVariable<int32> CVarEnabled(
 	TEXT("r.FSP"),
 	1,
@@ -13,19 +12,52 @@ static TAutoConsoleVariable<int32> CVarEnabled(
 	TEXT(" 0: disabled\n")
 	TEXT(" 1: enabled (default)"));
 
-static TAutoConsoleVariable<float> CVarEffectStrenght(
-	TEXT("r.FSP.EffectStrenght"),
+static TAutoConsoleVariable<float> CVarOpacity(
+	TEXT("r.FSP.Opacity"),
 	0.1f,
-	TEXT("Controls an amount of depth buffer blending to the base color."));
+	TEXT("Fog opacity multiplier."));
+
+static TAutoConsoleVariable<float> CVarFogStart(
+	TEXT("r.FSP.FogStart"),
+	500.0f,
+	TEXT("Distance at which fog starts."));
+
+static TAutoConsoleVariable<float> CVarFogCurveMin(
+	TEXT("r.FSP.FogCurveMin"),
+	0.0f,
+	TEXT("Fog smoothstep min distance."));
+
+static TAutoConsoleVariable<float> CVarFogCurveMax(
+	TEXT("r.FSP.FogCurveMax"),
+	1000.0f,
+	TEXT("Fog smoothstep max distance."));
+
+static TAutoConsoleVariable<float> CVarFogColorR(
+	TEXT("r.FSP.FogColorR"),
+	0.5f,
+	TEXT("Fog color R."));
+
+static TAutoConsoleVariable<float> CVarFogColorG(
+	TEXT("r.FSP.FogColorG"),
+	0.5f,
+	TEXT("Fog color G."));
+
+static TAutoConsoleVariable<float> CVarFogColorB(
+	TEXT("r.FSP.FogColorB"),
+	0.5f,
+	TEXT("Fog color B."));
 
 
-FFullScreenPassSceneViewExtension::FFullScreenPassSceneViewExtension(const FAutoRegister& AutoRegister) :
-	FSceneViewExtensionBase(AutoRegister)
+FFullScreenPassSceneViewExtension::FFullScreenPassSceneViewExtension(const FAutoRegister& AutoRegister)
+	: FSceneViewExtensionBase(AutoRegister)
 {
-
 }
 
-void FFullScreenPassSceneViewExtension::PrePostProcessPass_RenderThread(FRDGBuilder& GraphBuilder, const FSceneView& View, const FPostProcessingInputs& Inputs)
+void FFullScreenPassSceneViewExtension::PrePostProcessPass_RenderThread(
+	FRDGBuilder& GraphBuilder,
+	const FSceneView& View,
+	const FPostProcessingInputs& Inputs
+)
 {
 	if (CVarEnabled->GetInt() == 0)
 	{
@@ -35,7 +67,6 @@ void FFullScreenPassSceneViewExtension::PrePostProcessPass_RenderThread(FRDGBuil
 	Inputs.Validate();
 
 	const FIntRect PrimaryViewRect = UE::FXRenderingUtils::GetRawViewRectUnsafe(View);
-
 	FScreenPassTexture SceneColor((*Inputs.SceneTextures)->SceneColorTexture, PrimaryViewRect);
 
 	if (!SceneColor.IsValid())
@@ -46,22 +77,32 @@ void FFullScreenPassSceneViewExtension::PrePostProcessPass_RenderThread(FRDGBuil
 	const FScreenPassTextureViewport Viewport(SceneColor);
 
 	FRDGTextureDesc SceneColorDesc = SceneColor.Texture->Desc;
-
 	SceneColorDesc.Format = PF_FloatRGBA;
-	FLinearColor ClearColor(0., 0., 0., 0.);
-	SceneColorDesc.ClearValue = FClearValueBinding(ClearColor);
+	SceneColorDesc.ClearValue = FClearValueBinding(FLinearColor(0.f, 0.f, 0.f, 0.f));
 
-	FRDGTexture* ResultTexture = GraphBuilder.CreateTexture(SceneColorDesc, TEXT("FulllScreenPassResult"));
-	FScreenPassRenderTarget ResultRenderTarget = FScreenPassRenderTarget(ResultTexture, SceneColor.ViewRect, ERenderTargetLoadAction::EClear);
-	
+	FRDGTexture* ResultTexture = GraphBuilder.CreateTexture(SceneColorDesc, TEXT("FullScreenPassResult"));
+	FScreenPassRenderTarget ResultRenderTarget(ResultTexture, SceneColor.ViewRect, ERenderTargetLoadAction::EClear);
+
 	FGlobalShaderMap* GlobalShaderMap = GetGlobalShaderMap(GMaxRHIFeatureLevel);
-	TShaderMapRef<FFullScreenPassVS> ScreenPassVS(GlobalShaderMap);
+	TShaderMapRef<FScreenPassVS> ScreenPassVS(GlobalShaderMap);
 	TShaderMapRef<FFullScreenPassPS> ScreenPassPS(GlobalShaderMap);
 
 	FFullScreenPassPS::FParameters* Parameters = GraphBuilder.AllocParameters<FFullScreenPassPS::FParameters>();
 	Parameters->View = View.ViewUniformBuffer;
 	Parameters->SceneTexturesStruct = Inputs.SceneTextures;
-	Parameters->Strenght = CVarEffectStrenght->GetFloat();
+
+	Parameters->Opacity = CVarOpacity->GetFloat();
+	Parameters->FogStart = CVarFogStart->GetFloat();
+	Parameters->FogCurve = FVector2f(
+		CVarFogCurveMin->GetFloat(),
+		CVarFogCurveMax->GetFloat()
+	);
+	Parameters->FogColor = FVector4f(
+		CVarFogColorR->GetFloat(),
+		CVarFogColorG->GetFloat(),
+		CVarFogColorB->GetFloat(),
+		1.0f
+	);
 
 	Parameters->RenderTargets[0] = ResultRenderTarget.GetRenderTargetBinding();
 
